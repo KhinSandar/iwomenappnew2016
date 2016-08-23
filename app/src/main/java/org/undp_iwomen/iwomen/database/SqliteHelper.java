f58888bba5d980df1ccfab2b723c8be0345ef45e
KhinSandar <Message>
2016-08-23 17:52:24 +0630

    Update in BeKnowledgeable UI

--- app/src/main/java/org/undp_iwomen/iwomen/database/SqliteHelper.java
+++ app/src/main/java/org/undp_iwomen/iwomen/database/SqliteHelper.java
@@ -10,7 +10,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class SqliteHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "IWOMENDB";
-    private static int DATABASE_VERSION = 6;//Version Code 5 in 4 , Version Code 6 in 5
+    private static int DATABASE_VERSION = 6;//Version Code 5 in 4 , Version Code 6 in 5 ,Versison code 7 6
 
 
     public SqliteHelper(Context context) {
